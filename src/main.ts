187ee73b6302cd9c1cbfe8e81aba09f18161f566
Ufuk Seyithan Erdem <usethemasea@gmail.com>
2024-08-21 23:25:47 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,20 @@ import Foo from "./foo";
 const foo = new Foo();
 
 msg("Hello from main.ts");
+
+declare enum DropHookReturn { 
+    DropHookDoNotDrop = 1
+}
+
+declare var __HOOKS__: Record<string, Function>
+
+__HOOKS__ = {}
+
+__HOOKS__.drop = (playerId: PlayerID): DropHookReturn | void => {
+    msg2(playerId, 'dropped by ' + player(playerId, 'name'))
+
+    return DropHookReturn.DropHookDoNotDrop
+}
+
+addhook('drop', '__HOOKS__.drop')
+
